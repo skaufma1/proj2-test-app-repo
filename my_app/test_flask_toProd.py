4771a6df935836f2ccda8a90f1deed3e6ae93ebf
skaufma1 <123653249+skaufma1@users.noreply.github.com>
2023-06-12 18:02:40 +0300

    Update test_flask_toProd.py

--- my_app/test_flask_toProd.py
+++ my_app/test_flask_toProd.py
@@ -41,7 +41,7 @@ def test_signup_1(setup):
     """
 
     try:
-        assert "Wellllcome Bruce Springsteen" in message
+        assert "Welcome Bruce Springsteen" in message
         print("Test result: Success")
 
         # Execute the SQL statement
